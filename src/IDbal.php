92e03e10ea736288c8ede284bd1975235c94bdb1
Jaroslav Hranička <hranicka@outlook.com>
2016-05-18 10:30:08 +0200

    Typos

--- src/IDbal.php
+++ src/IDbal.php
@@ -18,43 +18,43 @@ interface IDbal
 {
 
 	/**
-	 * @param  string $sql
+	 * @param string $sql
 	 * @return array list of rows represented by assoc. arrays
 	 */
 	public function query(string $sql);
 
 	/**
-	 * @param  string $sql
+	 * @param string $sql
 	 * @return int number of affected rows
 	 */
 	public function exec(string $sql);
 
 	/**
-	 * @param  string $value
+	 * @param string $value
 	 * @return string escaped string wrapped in quotes
 	 */
 	public function escapeString(string $value);
 
 	/**
-	 * @param  int $value
+	 * @param int $value
 	 * @return string
 	 */
 	public function escapeInt(int $value);
 
 	/**
-	 * @param  bool $value
+	 * @param bool $value
 	 * @return string
 	 */
 	public function escapeBool(bool $value);
 
 	/**
-	 * @param  DateTime $value
+	 * @param DateTime $value
 	 * @return string
 	 */
 	public function escapeDateTime(DateTime $value);
 
 	/**
-	 * @param  string $value
+	 * @param string $value
 	 * @return string
 	 */
 	public function escapeIdentifier(string $value);
